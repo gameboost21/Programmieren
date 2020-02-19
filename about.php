2d6d63cc45cd5a7253250b9eca4680802382f193
gameboost21 <51078727+gameboost21@users.noreply.github.com>
2020-02-19 10:35:23 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="./style.css" />
+  <title>About</title>
+</head>
+<body>
+  <div class="about_intro">
+  <h1>About</h1>
+  <p>Welcome to the About Page!<br>
+    My Name is Tom Uschkoreit. I'm 19 Years old.<br>
+    Currently I am learning to create my own website using HTML, CSS, JavaScript and PHP.</p>
+    <br>
+    <p>Go back to <a href="./index.php">Homepage</a></p>
+    <p>Go to the <a href="./calendar.php">Google Calendar</a></p>
+    <img class="bild2" src="bild2.png">
+  </div>
+</body>
+</html>
